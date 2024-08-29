8d5318d94262675358d7f224b80d684cec3ebdd4
Ameer Ahmed <aahmed335467@gmail.com>
2024-08-29 21:09:42 +0100

    Add project files.

---
+++ ObjectDetector/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ObjectDetector
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
