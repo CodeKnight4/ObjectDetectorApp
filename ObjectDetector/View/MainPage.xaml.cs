f5f29c78b57e207ee4a9ffb93faaaa782d2fa6ed
Ameer Ahmed <aahmed335467@gmail.com>
2024-09-02 15:21:35 +0100

    Added classes for future development and integrated device camera functionality.

---
+++ ObjectDetector/View/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using CommunityToolkit.Maui.Camera;
+namespace ObjectDetector
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCameraClicked(object sender, EventArgs e)
+        {
+            //count++;
+
+            //if (count == 1)
+            //    CameraBtn.Text = $"Clicked {count} time";
+            //else
+            //    CameraBtn.Text = $"Clicked {count} times";
+
+            //SemanticScreenReader.Announce(CameraBtn.Text);
+        }
+    }
+
+}
