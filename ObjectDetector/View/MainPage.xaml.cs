6eb13c1bdcebdab60741011c193a046ef29f1f9d
Ameer Ahmed <aahmed335467@gmail.com>
2024-09-03 20:26:42 +0100

    Changed icon for button from string unicode character to .svg image. Also added implementation to show/hide video capture window.

--- ObjectDetector/View/MainPage.xaml.cs
+++ ObjectDetector/View/MainPage.xaml.cs
@@ -3,21 +3,40 @@ namespace ObjectDetector
 {
     public partial class MainPage : ContentPage
     {
+        bool isCameraEnabled = false;
+
         public MainPage()
         {
             InitializeComponent();
+            CameraWindow.IsVisible = false;
         }
 
-        private void OnCameraClicked(object sender, EventArgs e)
+        private async void OnCameraClicked(object sender, EventArgs e)
         {
-            //count++;
+            CameraBtn.IsEnabled = false;
 
-            //if (count == 1)
-            //    CameraBtn.Text = $"Clicked {count} time";
-            //else
-            //    CameraBtn.Text = $"Clicked {count} times";
+            await ToggleCameraBtn();
 
-            //SemanticScreenReader.Announce(CameraBtn.Text);
+            CameraBtn.IsEnabled = true;
+        }
+
+        private async Task ToggleCameraBtn()
+        {
+            isCameraEnabled = !isCameraEnabled;
+
+            if (isCameraEnabled)
+            {
+                CameraBtn.ImageSource = "video_slash_solid.png";
+                CameraWindow.IsVisible = true;
+            }
+            else
+            {
+                CameraBtn.ImageSource = "video_solid.png";
+                CameraWindow.IsVisible = false;
+
+            }
+
+            await Task.Delay(500); // Give a second to the program to setup the device camera
         }
     }
 
